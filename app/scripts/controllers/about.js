3c511d774da94b698c048d12cc4df6326009a64e
Pakkapon Phongthawee <me@pureapp.in.th>
2016-12-02 07:46:13 +0700

    init: yo generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kunsubApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kunsubApp
+ */
+angular.module('kunsubApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kunsubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kunsubApp
+ */
+angular.module('kunsubApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
